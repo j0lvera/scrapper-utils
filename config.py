169d1d30c9657336c0d0c151103b894ccee48d32
thinkxl <jolvera@posteo.de>
2018-06-05 23:44:46 -0500

    remove reddit leftovers

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
 import os
 
-REDDIT_API_ID = os.getenv('REDDIT_API_ID', '')
-REDDIT_API_SECRET = os.getenv('REDDIT_API_SECRET', '')
 USER_AGENT = os.getenv('USER_AGENT', '')
-SUBREDDIT = os.getenv('SUBREDDIT', '/')
 DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///database.sqlite')
